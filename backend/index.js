dce532c983be275de033ecdc14b4b9be8b300134
paavkar <paavo_karppinen@outlook.com>
2023-01-30 11:53:07 +0000

    Testing the deployment of the start of MERN app

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,25 @@
+const path = require('path')
 const express = require('express')
 const app = express()
+const cors = require('cors')
+require("dotenv").config()
+
+var corsOptions = {
+  origin: process.env.CLIENT_ORIGIN || "http://localhost:3000"
+}
+app.use(cors(corsOptions))
+app.use(express.static(path.join(__dirname, './build')))
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-})
+  res.sendFile(path.join(__dirname, './build/index.html'));
+});
 
 
 app.get('/server-info', (req, res) => {
     res.send('<p>This will show the same server info as the Python file app.py</p>')
 })
 
-const PORT = 5001
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
