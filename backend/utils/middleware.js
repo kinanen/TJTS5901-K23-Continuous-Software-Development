28438ff643362df8413fd56891c5d6506e44060d
paaakarp <paaakarp@jyu.fi>
2023-02-22 11:04:25 +0200

    Update backend/utils/middleware.js

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -6,7 +6,7 @@ const userExtractor = async (request, response, next) => {
   const authorization = request.get('authorization')
 
   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    const decodedToken = jwt.verify(authorization.substring(7), process.env.SECRET)
+    const decodedToken = jwt.verify(authorization.substring(7), process.env.SECRET || "supersalaistatestaamista")
     if (decodedToken) {
       request.user = await User.findById(decodedToken.id)
     }
